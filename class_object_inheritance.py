046d5fc0ecf7091871d8a0073fd1a3a5fc0afd39
anildalar <dollorinfotech@gmail.com>
2023-12-11 08:57:57 +0530

    new

---
+++ class_object_inheritance.py
@@ -0,0 +1,27 @@
+#1. Class Defination
+
+
+
+class Car: # PascleCase
+    #1.1 Property = Variable
+    brand="TATA"
+    model='2024' # variable
+    
+    #1.2 Constructor
+    
+    
+    #1.3 Method = Functions
+    def getMyBrand(self): # camleCase
+        # self is internal class object
+        print(f"Brand is {self.brand}")
+        print(f"Model year is {self.model}")
+        pass
+    
+    pass
+#2 Create Class Object
+# classObject = ClassName()
+#co is external class object
+co = Car()
+
+# Call the method with classObjectcls
+co.getMyBrand()
